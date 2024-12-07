ad4593551c49804f33689127883d3735258d610c
wille <wilma154.student.liu.se>
2024-12-07 17:45:35 +0100

    day 7 done

---
+++ Day 7/day7_1.py
@@ -0,0 +1,33 @@
+from itertools import product
+
+with open("input_7.txt", "r") as file:
+    lines = [list(line.split(" ")) for line in file.readlines()]
+
+cal_result = 0
+values = []
+numbers = []
+for eq in lines:
+    cur_nums = []
+    for i in range(len(eq)):
+        if i == 0:
+            values.append(int(eq[i][:-1]))
+        else:
+            cur_nums.append(int(eq[i]))
+    numbers.append(cur_nums)
+
+def check_eq(nums, val, op):
+    temp_val = nums[0]
+    for i in range(len(nums)-1):
+        if op[i] == "+":
+            temp_val += nums[i+1]
+        else:
+            temp_val *= nums[i+1]
+    return temp_val == val
+
+for i in range(len(numbers)):
+    ops = list(product(["+", "*"], repeat=len(numbers[i])-1))
+    for op in ops:
+        if check_eq(numbers[i], values[i], op):
+            cal_result += values[i]
+            break
+print(cal_result)
